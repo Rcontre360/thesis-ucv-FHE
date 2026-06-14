400a84f85cddcef001348e6c80aabba10fdd46b1
Rcontre360 <rcontreraspimentel@gmail.com>
2026-06-14 11:21:28 -0400

    example of server-client communication

---
+++ sdk/examples/server/client.py
@@ -0,0 +1,16 @@
+from collections.abc import Callable
+from fhe_ml import Client, EncryptedVector, FHEContext
+
+
+def setup_client(context: FHEContext) -> Client:
+    return Client(context)
+
+
+def send_request(
+    client: Client,
+    server: Callable[[EncryptedVector], EncryptedVector],
+    values: list[float],
+) -> list[float]:
+    encrypted = client.encrypt(values)
+    encrypted_result = server(encrypted)
+    return client.decrypt(encrypted_result)
